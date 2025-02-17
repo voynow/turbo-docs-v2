a8d2ef4aa7f2cc36797eed9e2888511d9b6fd63c
voynow <voynow99@gmail.com>
2025-02-17 12:55:24 -0500

    re releasing td 🫡

---
+++ turbo_docs/cli.py
@@ -0,0 +1,13 @@
+"""Command line interface for turbo-docs."""
+
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    """Entry point for the turbo-docs CLI."""
+    print("YO YO YO!!!!!!")
+    raise SystemExit(0)
+
+
+if __name__ == "__main__":
+    main()
